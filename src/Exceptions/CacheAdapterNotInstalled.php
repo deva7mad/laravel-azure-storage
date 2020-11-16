927f59c63141043fd97ef4fbe3969f1997a8ba14
Deva7mad <dev.elknany@gmail.com>
2020-11-16 15:11:58 +0200

    init

---
+++ src/Exceptions/CacheAdapterNotInstalled.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Deva7mad\LaravelAzureStorage\Exceptions;
+
+/**
+ * Invalid custom URL
+ */
+final class CacheAdapterNotInstalled extends \Exception
+{
+}
---
+++ src/Exceptions/InvalidCustomUrl.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Deva7mad\LaravelAzureStorage\Exceptions;
+
+/**
+ * Invalid custom URL
+ */
+final class InvalidCustomUrl extends \Exception
+{
+}
